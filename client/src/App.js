eb6ae46feeb46fb2ef89d99e234d4bae8eb8b8e1
NedDinev <ned.dinev@gmail.com>
2023-07-30 18:03:00 +0300

    Create navigation bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import ProductList from "./components/ProductList/ProductList";
 import { Container } from "react-bootstrap";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
   return (
     <div>
-      <header>
-        <a href="/">Jewelry Shop</a>
+      <header className="sticky-top">
+        <NavigationBar />
       </header>
       <main>
         <Container className="mt-5">
---
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { data } from "../../data";
+
+export default function NavigationBar() {
+  const categories = [
+    ...new Set(data.products.map((product) => product.category)),
+  ]; // Extracting unique categories from the data
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="/images/C4-Jewelry-Logo.png"
+            className="logo"
+            alt="C4 Jewelry"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto w-100 justify-content-end ">
+            {categories.map((category) => (
+              <Nav.Item
+                key={category}
+                as={Link}
+                to={`/${category}`}
+                className="nav-link text-center"
+              >
+                {category.toUpperCase()}
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
